413d981803e00c01eb7d2bfcfba50aaf77b25b0c
Nolan Kovacik <noltron000@gmail.com>
2019-04-02 17:29:06 -0700

    fixing css grid

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,21 +1,24 @@
 import React, { Component } from 'react'
 
 import '../css/app.css'
+import Head from '../jsx/head.jsx'
 import Next from '../jsx/next.jsx'
 import Board from '../jsx/board.jsx'
 import Score from '../jsx/score.jsx'
+import Controls from '../jsx/controls.jsx'
 
-class app extends Component {
+class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>hello world</h1>
-				<Board />
+			<div className='app'>
+				<Head />
 				<Next />
+				<Board />
 				<Score />
+				<Controls />
 			</div>
 		)
 	}
 }
 
-export default app
+export default App
---
+++ src/jsx/controls.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class Controls extends Component {
+
+	render() {
+		return (
+			<div className="controls">
+				{/* left */}
+				<button onClick={(e) => {
+
+				}}>Left</button>
+
+				{/* right */}
+				<button onClick={(e) => {
+
+				}}>Right</button>
+
+				{/* down */}
+				<button onClick={(e) => {
+
+				}}>Down</button>
+
+				{/* spinL */}
+				<button onClick={(e) => {
+
+				}}>Rotate Left</button>
+
+				{/* spinR */}
+				<button onClick={(e) => {
+
+				}}>Rotate Right</button>
+
+			</div>
+		)
+	}
+}
+
+export default Controls
