802b7d986f46efd2e20ebb324a6e52e58c017ad9
Nolan Kovacik <noltron000@gmail.com>
2019-04-04 12:59:35 -0700

    added store and provider

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import reducers from '../reducers/'
 
 import '../css/app.css'
 import Head from '../jsx/head.jsx'
@@ -8,17 +12,21 @@ import Score from '../jsx/score.jsx'
 import Controls from '../jsx/controls.jsx'
 import Overlay from '../jsx/overlay.jsx'
 
+const store = createStore(reducers)
+
 class App extends Component {
 	render() {
 		return (
-			<div className='app'>
-				<Head />
-				<Next />
-				<Board />
-				<Score />
-				<Controls />
-				<Overlay />
-			</div>
+			<Provider store={store}>
+				<div className='app'>
+					<Head />
+					<Next />
+					<Board />
+					<Score />
+					<Controls />
+					<Overlay />
+				</div>
+			</Provider>
 		)
 	}
 }
