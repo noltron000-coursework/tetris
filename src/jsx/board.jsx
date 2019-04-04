cdcd73d5befb8a8f27ba77c12e60ef362e0717c9
Nolan Kovacik <noltron000@gmail.com>
2019-04-04 14:39:25 -0700

    connecting game board

--- src/jsx/board.jsx
+++ src/jsx/board.jsx
@@ -1,20 +1,67 @@
 import React, { Component } from 'react'
+
+import { connect } from 'react-redux'
+
 import Square from '../jsx/square.jsx'
+
+import { move_down } from '../actions/'
+import { shapes } from '../utilities/'
+
+
+const map_state_to_props = (state) => {
+	return {
+		grid: state.game.grid,
+		shape: state.game.shape,
+		rotation: state.game.rotation,
+		x: state.game.x,
+		y: state.game.y,
+		speed: state.game.speed,
+		isRunning: state.game.isRunning
+	}
+}
+
+// Map Dipatch to Props
+const map_dispatch_to_props = () => {
+	return {
+		move_down
+	}
+}
 
 // Represents a 10 x 18 grid of grid squares
 class Board extends Component {
 
 	// generates an array of 18 rows, each containing 10 Squares.
 	make_grid() {
-		const grid = []
-		for (let row = 0; row < 18; row ++) {
-			grid.push([])
-			for (let col = 0; col < 10; col ++) {
-				grid[row].push(<Square key={`${col}${row}`} color="1" />)
-			}
-		}
+		// collect properties mapped to props from state.
+		const { grid, shape, rotation, x, y } = this.props
+		// get the block which is the current shape the player is controlling
+		const block = shapes[shape][rotation]
+		const blockColor = shape
+		// map rows
+		return grid.map((rowArray, row) => {
+			// map columns
+			return rowArray.map((square, col) => {
+				// Find the block x and y on the shape grid
+				// By subtracting the x and y from the col and the row we get the position of the upper left corner of the block array as if it was superimposed over the main grid
+				const blockX = col - x
+				const blockY = row - y
+				let color = square
 
-		return grid
+				// Map current falling block to grid.
+				// For any squares that fall on the grid we need to look at the block array and see if there is a 1 in this case we use the block color.
+				if (blockX >= 0 && blockX < block.length && blockY >= 0 && blockY < block.length) {
+					color = block[blockY][blockX] === 0 ? color : blockColor
+				}
+				// Generate a unique key for every block
+				const k = row * grid[0].length + col;
+				// Generate a grid square
+				return <Square
+								key={k}
+								square={square}
+								color={color}>{square}
+							</Square>
+			})
+		})
 	}
 
 	// The components generated in make_grid are rendered in div.board
@@ -27,4 +74,5 @@ class Board extends Component {
 	}
 }
 
-export default Board
+// Connect the component
+export default connect(map_state_to_props, map_dispatch_to_props())(Board)
