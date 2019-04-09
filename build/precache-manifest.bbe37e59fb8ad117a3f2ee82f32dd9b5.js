66912cf4802bb2eeeed5c18f1c65864ab21d860c
Nolan Kovacik <noltron000@gmail.com>
2019-04-09 15:07:21 -0700

    electronifying the app

---
+++ build/precache-manifest.bbe37e59fb8ad117a3f2ee82f32dd9b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "83ef52564070ae071c64",
+    "url": "./static/js/main.fbe22f82.chunk.js"
+  },
+  {
+    "revision": "869bfdfeca314604db11",
+    "url": "./static/js/2.6484c9ba.chunk.js"
+  },
+  {
+    "revision": "83ef52564070ae071c64",
+    "url": "./static/css/main.4303e1c7.chunk.css"
+  },
+  {
+    "revision": "937aade6b08a4d2c940792654fff09cf",
+    "url": "./index.html"
+  }
+];
